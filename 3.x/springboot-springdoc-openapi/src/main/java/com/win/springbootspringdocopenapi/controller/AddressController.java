2337d0e15a4378911c42cac2756c547a2c589970
ly2011 <1518550424@qq.com>
2024-01-11 23:08:46 +0800

    feat: 添加 springdoc-openapi-starter-webmvc-ui 和 入参案例

--- 3.x/springboot-springdoc-openapi/src/main/java/com/win/springbootspringdocopenapi/controller/AddressController.java
+++ 3.x/springboot-springdoc-openapi/src/main/java/com/win/springbootspringdocopenapi/controller/AddressController.java
@@ -7,14 +7,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.win.springbootspringdocopenapi.entity.param.AddressParam;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "地址模块")
 @RestController
 @RequestMapping("/address")
 public class AddressController {
   /**
    * http://localhost:3003/address/add
+   * http://localhost:3003/swagger-ui/index.html
    * @param addressParam
    * @return address
    */
+  @Operation(summary = "添加地址", description = "根据地址参数添加地址")
+  @Parameters({
+    @Parameter(name = "city",description = "城市编码", in = ParameterIn.QUERY, required = true),
+    @Parameter(name = "zipCode",description = "城市编码", in = ParameterIn.QUERY, required = true)
+  })
   @PostMapping("add")
   public ResponseEntity<String> add(AddressParam addressParam) {
     return ResponseEntity.ok("success");
--- 3.x/springboot-springdoc-openapi/src/main/java/com/win/springbootspringdocopenapi/controller/UserController.java
+++ 3.x/springboot-springdoc-openapi/src/main/java/com/win/springbootspringdocopenapi/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,14 +15,23 @@ import com.win.springbootspringdocopenapi.entity.param.UserParam;
 import com.win.springbootspringdocopenapi.entity.vo.AddressVo;
 import com.win.springbootspringdocopenapi.entity.vo.UserVo;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "用户模块")
 @RestController
 @RequestMapping("/user")
 public class UserController {
   /**
    * http://localhost:3003/user/add
+   * http://localhost:3003/swagger-ui/index.html
    * @param userParam
    * @return user
    */
+  @Operation(summary = "添加用户", description = "根据用户参数添加用户")
+  @Parameter()
   @PostMapping("add")
   public ResponseEntity<String> add(@RequestBody UserParam userParam) {
     return ResponseEntity.ok("添加用户成功");
@@ -29,11 +39,26 @@ public class UserController {
 
   /**
    * http://localhost:3003/user/list
+   * http://localhost:3003/swagger-ui/index.html
    * @return user list
    */
+  @Operation(summary = "查询用户列表", description = "查询用户列表")
   @GetMapping("list")
   public ResponseEntity<List<UserVo>> list() {
     List<UserVo> userVoList = Collections.singletonList(UserVo.builder().name("win").age(16).address(AddressVo.builder().city("SZ").zipcode("10001").build()).build());
     return ResponseEntity.ok(userVoList);
+  }
+
+  /**
+   * http://localhost:3003/user/find/win
+   * @param name
+   * @return user
+   */
+  @Operation(summary = "根据用户名查询用户", description = "根据用户名查询用户")
+  @Parameter(name = "name",description = "用户名", in = ParameterIn.PATH)
+  @GetMapping("find/{name}")
+  public ResponseEntity<UserVo> find(@Parameter(description = "用户名") @PathVariable String name) {
+    UserVo userVo = UserVo.builder().name(name).age(16).address(AddressVo.builder().city("SZ").zipcode("10001").build()).build();
+    return ResponseEntity.ok(userVo);
   }
 }
